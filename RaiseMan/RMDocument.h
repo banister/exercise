d4fbc8e0699e945224037e7948285c0bf2802b9c
John Mair <jrmair@gmail.com>
2012-03-17 14:03:09 +1300

    Initial Commit

---
+++ RaiseMan/RMDocument.h
@@ -0,0 +1,13 @@
+//
+//  RMDocument.h
+//  RaiseMan
+//
+//  Created by John Mair on 17/03/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface RMDocument : NSDocument
+
+@end
